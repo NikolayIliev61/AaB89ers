ef405663870dd8596ba5dc1586a7da9653116b31
Filip Komogorov <filipkomogorov@gmail.com>
2021-06-05 18:57:36 +0200

    initial code for main page layedout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
+// ===================== WordPress CONNECTION ====================
+
+// getting the auth token for private posts using WP admin credentials
+function getToken(){
+    // fetching for the token by using the jwt-auth extension in WP
+    fetch('https://www.buckledown.dk/wp-json/jwt-auth/v1/token',{
+        // corecting the default GET to POST method
+        method: 'POST',
+        // passing the WP admin credentials
+        body: JSON.stringify( {
+            username: 'api.user',
+            password: 'API-key-1234#!'
+        }),
+        
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    // making the response JSON format
+    .then(response => response.json())
+    // saving the token into the local stage
+    .then (response => window.localStorage.setItem('authToken', response.token))
+}
+// calling the function
+getToken()
+
+
+// fetching all private posts
+function getData(){
+    // fetch all private posts from the WP server
+    fetch('https://www.buckledown.dk/wp-json/wp/v2/posts?status=private', {
+        // adding authorization header with the token taken from the local storage
+        headers: {
+            "Authorization" : `Bearer ${window.localStorage.getItem('authToken')}`
+        }
+    })
+    // make the reponse into JSON format
+    .then(response => response.json())
+    .then(response => console.log(response))
+
+}
+
+getData()
+
+
+
+// ================ BODY ==================
 const bodyHeader = document.querySelector('#bodyHeader');
 
 bodyHeader.innerHTML = `
@@ -16,37 +63,3 @@ bodyHeader.innerHTML = `
 </div>
 
 `;
-
-// var username = "api.user";
-var secret = "API-key-1234#!";
-console.log(window.localStorage.getItem('authToken'))
-
-function getToken(){
-    fetch('https://www.buckledown.dk/wp-json/jwt-auth/v1/token',{
-        method: 'POST',
-        body: JSON.stringify( {
-            username: 'api.user',
-            password: 'API-key-1234#!'
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then (response => window.localStorage.setItem('authToken', response.token))
-}
-
-getToken()
-
-function getData(){
-    fetch('https://www.buckledown.dk/wp-json/wp/v2/posts?status=private', {
-        headers: {
-            "Authorization" : `Bearer ${window.localStorage.getItem('authToken')}`
-        }
-    })
-    .then(response => response.json())
-    .then(response => console.log(response))
-
-}
-
-getData()
