58e5d7f274883c13eb97a663ce80f076db4a8406
Filip Komogorov <filipkomogorov@gmail.com>
2021-06-05 16:26:11 +0200

    initial code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const bodyHeader = document.querySelector('#bodyHeader');
+
+bodyHeader.innerHTML = `
+
+<div class="wrapper header-wrapper">
+<img class="logo" src="assets/media/logo.png">
+<ul>
+    <li>GET STARTED</li>
+    <li>TEAMS</li>
+    <li>HISTORY</li>
+    <li>CONTACTS</li>
+    <li><i class="fab fa-facebook-square fa-lg"></i></li>
+    <li><i class="fab fa-instagram-square fa-lg"></i></li>
+</ul>
+
+</div>
+
+`;
+
