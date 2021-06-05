b257c8523b8047f72eefcb27217b8d092ff58910
Filip Komogorov <filipkomogorov@gmail.com>
2021-06-05 18:04:42 +0200

    JS connection to WP completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const bodyHeader = document.querySelector('#bodyHeader');
 
 bodyHeader.innerHTML = `
 
-<div class="wrapper header-wrapper">
+<div class="header-wrapper">
 <img class="logo" src="assets/media/logo.png">
 <ul>
     <li>GET STARTED</li>
@@ -17,3 +17,36 @@ bodyHeader.innerHTML = `
 
 `;
 
+// var username = "api.user";
+var secret = "API-key-1234#!";
+console.log(window.localStorage.getItem('authToken'))
+
+function getToken(){
+    fetch('https://www.buckledown.dk/wp-json/jwt-auth/v1/token',{
+        method: 'POST',
+        body: JSON.stringify( {
+            username: 'api.user',
+            password: 'API-key-1234#!'
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then (response => window.localStorage.setItem('authToken', response.token))
+}
+
+getToken()
+
+function getData(){
+    fetch('https://www.buckledown.dk/wp-json/wp/v2/posts?status=private', {
+        headers: {
+            "Authorization" : `Bearer ${window.localStorage.getItem('authToken')}`
+        }
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+
+}
+
+getData()
