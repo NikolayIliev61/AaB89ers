b4bbe3d9ffeff5f9fdd7581b924d22631a81c5f0
Filip Komogorov <filipkomogorov@gmail.com>
2021-06-07 15:44:32 +0200

    updates on the upcoming matches

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// ================ BODY ==================
+const bodyHeader = document.querySelector('#bodyHeader');
+
+bodyHeader.innerHTML = `
+
+<div class="header-wrapper">
+<img class="logo" src="assets/media/logo.png">
+<ul>
+    <li>GET STARTED</li>
+    <li>TEAMS</li>
+    <li>HISTORY</li>
+    <li>CONTACTS</li>
+    <li><i class="fab fa-facebook-square fa-lg"></i></li>
+    <li><i class="fab fa-instagram-square fa-lg"></i></li>
+</ul>
+
+</div>
+
+`;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// ===================== VARIABLES ====================
+
+let program = document.querySelector('#programWrapper');
+
+
 // ===================== WordPress CONNECTION ====================
 
 // getting the auth token for private posts using WP admin credentials
@@ -27,6 +32,7 @@ getToken()
 
 // fetching all private posts
 function getData(){
+    
     // fetch all private posts from the WP server
     fetch('https://www.buckledown.dk/wp-json/wp/v2/posts?status=private', {
         // adding authorization header with the token taken from the local storage
@@ -36,30 +42,34 @@ function getData(){
     })
     // make the reponse into JSON format
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => response.forEach(function(el, index){
+        if(el.acf.typeOfPost == 'game'){
+            console.log(el)
+            program.innerHTML += `
+            <div class = "game game--${index+1}" style="width:100%; height:100%; position: absolute; left:${index*100}%; top:0;">
+                <div class="bg" style = "width:100%; height: 100%; position: absolute; top: 0; left: 0; background-image: url(${el.acf.background_img.url}); background-position: center; background-size: cover; filter: brightness(25%);"></div>
+                <div class="first-team" style= "position: absolute; top: 10%; left: 15%;">
+                    <img style="justify-self: center" src="${el.acf.home_team_photo.sizes.thumbnail}">
+                    <h2 class="text-white team-name">${el.acf.home_team}</h2>
+                </div>
+                <div class="vs text-white">VS</div>
+                <div class="second-team" style= "position: absolute; top: 10%; right: 15%; ">
+                    <img  src = "${el.acf.guest_team_photo.sizes.thumbnail}" style="transform: scaleX(-1);">
+                    <h2 class="text-white team-name">${el.acf.guest_team_name}</h2>
+                </div>
+                <div class="time">
+                    <h3 class="text-white">${el.acf.date}</h3>
+                </div>
+                <div class="place text-white" >
+                    <h3>${el.acf.place_of_the_game}</h3>
+                </div>
+            </div>
+        `
+        }
+    }))
 
 }
 
 getData()
 
 
-
-// ================ BODY ==================
-const bodyHeader = document.querySelector('#bodyHeader');
-
-bodyHeader.innerHTML = `
-
-<div class="header-wrapper">
-<img class="logo" src="assets/media/logo.png">
-<ul>
-    <li>GET STARTED</li>
-    <li>TEAMS</li>
-    <li>HISTORY</li>
-    <li>CONTACTS</li>
-    <li><i class="fab fa-facebook-square fa-lg"></i></li>
-    <li><i class="fab fa-instagram-square fa-lg"></i></li>
-</ul>
-
-</div>
-
-`;
